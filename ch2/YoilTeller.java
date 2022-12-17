336ef53146a0e0926d7bb75308a5753c4d7f523f
SEONG NAMKUNG <castello@naver.com>
2022-12-17 17:26:25 +0900

    Update YoilTeller.java

--- ch2/YoilTeller.java
+++ ch2/YoilTeller.java
@@ -15,9 +15,6 @@ public class YoilTeller {
     //    public static void main(String[] args) {
     public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // 1. 입력
-//        String year = args[0];
-//        String month = args[1];
-//        String day = args[2];
         String year = request.getParameter("year");
         String month = request.getParameter("month");
         String day = request.getParameter("day");
@@ -34,8 +31,6 @@ public class YoilTeller {
         char yoil = " 일월화수목금토".charAt(dayOfWeek);
 
         // 3. 출력
-//        System.out.println(year + "년 " + month + "월 " + day + "일은 ");
-//        System.out.println(yoil + "요일입니다.");
         response.setContentType("text/html");    // 응답의 형식을 html로 지정
         response.setCharacterEncoding("utf-8");  // 응답의 인코딩을 utf-8로 지정
         PrintWriter out = response.getWriter();  // 브라우저로의 출력 스트림(out)을 얻는다.
