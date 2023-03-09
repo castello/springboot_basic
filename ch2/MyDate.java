210a7706381e26c224d745b704b37ec871d0af57
SEONG NAMKUNG <castello@naver.com>
2023-03-09 23:03:09 +0900

    Create MyDate.java

---
+++ ch2/MyDate.java
@@ -0,0 +1,31 @@
+package com.fastcampus.ch2;
+
+public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+}
